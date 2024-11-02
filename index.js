21d33772fa5865c8436c1970222653d605a75b9b
Divyapriya-K <diyapriya0428@gmail.com>
2024-11-02 19:28:51 +0530

    completed

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.status(200).send("welcome to our API(PSWD-RESET)")
+})
+
+const port = process.env.PORT ||4000;
+
+app.listen(port,()=>{
+    console.log("server started and running on the port at the moment");
+    
+})
